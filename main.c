ed3207ff4e561d5c1966c497bb91b61a149f3803
root <root@localhost.localdomain.net>
2022-12-14 17:20:31 +0800

    变量类型无空格化，方便反射

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-long long result = 5374240117044095916;
+long long result = 7249970239362560142;
 int main() {
   /* ===== Block 1 ===== */
-  char var_1_0 = -45;
-  unsigned long var_1_1 = 3223125160;
-  var_1_1 -= (((((var_1_0>>var_1_1)+(var_1_0&var_1_1))/(var_1_1%var_1_1))&((var_1_0%var_1_1)*(((var_1_0/var_1_1)+(var_1_1^var_1_0))<<(var_1_1|var_1_1))))%(((((var_1_1/var_1_1)>>(var_1_1^var_1_0))/((var_1_1&var_1_1)-(var_1_0>>var_1_1)))<<(((var_1_1/var_1_0)|(var_1_1>>var_1_0))/((var_1_1*var_1_0)<<(var_1_1/var_1_0))))*((((var_1_1<<var_1_1)-(var_1_1-var_1_0))<<((var_1_1+var_1_0)+(var_1_1&var_1_0)))*(var_1_1<<var_1_1))));
-  var_1_1 -= ((((((var_1_0^var_1_0)&(var_1_1>>var_1_0))>>((var_1_0*var_1_0)+(var_1_1&var_1_0)))*(var_1_1%var_1_0))*(var_1_0*var_1_1))+(var_1_0^var_1_1));
+  short var_1_0 = 31321;
+  int var_1_1 = -1189752394;
+  unsigned char var_1_2 = 49;
+  unsigned int var_1_3 = 3814136147;
+  var_1_3 &= ((var_1_3&var_1_2)<<(var_1_3^var_1_1));
   
   /* ===== Block 2 ===== */
-  int var_2_0 = 521320162;
-  unsigned int var_2_1 = 2754317270;
-  unsigned char var_2_2 = 39;
-  long long var_2_3 = 8093934059740008138;
-  var_2_1 >>= (((var_2_3^var_2_0)%((((var_1_0^var_2_2)/(var_2_1%var_2_0))|((var_1_1^var_2_2)/(var_2_3<<var_2_3)))%(var_2_3^var_1_1)))<<(var_2_2&var_2_1));
-  var_2_2 %= (var_2_1-var_2_3);
-  var_1_1 &= ((var_1_0^var_2_0)>>(var_2_0-var_2_1));
-  var_2_2 <<= ((((var_1_0%var_2_0)%(var_1_0|var_2_3))^(((var_2_0/var_2_3)+((var_2_2^var_2_3)%(var_2_1-var_2_3)))-(((var_2_0|var_1_1)&(var_2_1%var_1_1))<<((var_1_1+var_2_0)+(var_2_1>>var_2_2)))))^((((var_1_1%var_1_0)&((var_1_0+var_2_2)^(var_2_0%var_1_1)))^(((var_2_1-var_2_0)<<(var_2_3*var_2_3))*((var_1_1%var_1_1)/(var_1_0*var_2_1))))/(var_1_1^var_2_2)));
+  unsigned int var_2_0 = 2017366661;
+  int var_2_1 = -1981032977;
+  var_1_0 |= (var_1_2<<var_1_0);
+  var_1_3 |= (var_2_1>>var_1_2);
+  var_2_0 ^= (var_2_1*var_2_0);
   
-  result += ((((((var_2_3%var_2_3)&(var_2_3-var_1_1))>>(var_2_3-var_1_1))*((var_2_3|var_1_1)-((var_1_1|var_1_1)+(var_1_1^var_2_3))))^(var_2_3-var_1_1))|(((var_2_3+var_1_1)+((var_1_1*var_2_3)*(var_2_3&var_1_1)))-(((var_1_1*var_2_3)^((var_2_3>>var_1_1)<<(var_1_1*var_1_1)))/(var_2_3|var_1_1))));
+  /* ===== Block 3 ===== */
+  long var_3_0 = -1102872405;
+  int var_3_1 = -1970469698;
+  var_1_0 &= (((var_3_1/var_1_3)+(((var_1_3>>var_1_2)^((var_1_1/var_1_0)/(var_3_0%var_2_0)))%(((var_1_3|var_2_1)-(var_1_1|var_1_2))*((var_2_1<<var_1_1)-(var_1_0*var_2_1)))))<<(var_1_2<<var_3_1));
+  
+  result /= (var_2_1%var_3_0);
   printf("RESULT : %4X\n", result);
 }
